f20fa924bdf318825c958c85f07df9f1f5245407
joaluku101 <joseph.digifyke@gmail.com>
2022-10-13 09:31:27 +0300

    TextField

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,22 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class MainFrame extends JFrame {
     public void init(){
+
+    
+
+        JLabel name = new JLabel("Your Name Here");
+    name.setBounds(50,60,50,50);
+    add(name);
+
+        JTextField name_field = new JTextField();
+        name_field.setBounds(50,60,50,50);
+        add(name_field);
+
+    
         setTitle("Login");
         setSize(700, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
